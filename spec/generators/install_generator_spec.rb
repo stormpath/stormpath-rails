b39d3fb9136a36cee3c7adcde9c669d5202c8103
Denis Grankin <liquidautumn@gmail.com>
2013-02-14 13:22:06 +0200

    removed comment from generates stormpath.yml

--- spec/generators/install_generator_spec.rb
+++ spec/generators/install_generator_spec.rb
@@ -12,6 +12,6 @@ describe Stormpath::Rails::Generators::InstallGenerator do
   end
 
   it "creates configuration file" do
-    assert_file "config/stormpath.yml", "common:\n  stormpath_url: <%= ENV['STORMPATH_URL'] %>\n  #application: https://api.stormpath.com/v1/applications/<application id>\n\ndevelopment:\n  root: https://api.stormpath.com/v1/directories/<root directory id>\n\ntest:\n  root: https://api.stormpath.com/v1/directories/<root directory id>\n\nproduction:\n  root: https://api.stormpath.com/v1/directories/<root directory id>\n"
+    assert_file "config/stormpath.yml", "common:\n  stormpath_url: <%= ENV['STORMPATH_URL'] %>\n\ndevelopment:\n  root: https://api.stormpath.com/v1/directories/<root directory id>\n\ntest:\n  root: https://api.stormpath.com/v1/directories/<root directory id>\n\nproduction:\n  root: https://api.stormpath.com/v1/directories/<root directory id>\n"
   end
 end
