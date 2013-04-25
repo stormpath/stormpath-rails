264498da69e1c0e3a19d255dadacedf341138d18
Erin Swenson-Healey <erin@carbonfive.com>
2013-04-25 10:16:14 -0700

    Adding SimpleCov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,12 @@
+# Note: If SimpleCov starts after your application code is already loaded (via require),
+# it won't be able to track your files and their coverage! The SimpleCov.start must be
+# issued before any of your application code is required!
+require 'simplecov'
+SimpleCov.start
+
 Dir["./spec/support/**/*.rb"].sort.each {|f| require f}
 
 RSpec.configure do |config|
+  config.mock_framework = :rspec
   config.order = 'random'
 end
