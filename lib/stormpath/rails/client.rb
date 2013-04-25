c37132bb90fc4948b12ef103845e884a77de5c72
Erin Swenson-Healey <erin@carbonfive.com>
2013-04-25 11:45:46 -0700

    Removing usage of Config and replacing with ENV

--- lib/stormpath/rails/client.rb
+++ lib/stormpath/rails/client.rb
@@ -8,19 +8,19 @@ module Stormpath
         attr_accessor :connection
       end
 
-      def self.authenticate_account(login, password)
-        application = self.ds.get_resource Config[:stormpath_url], Stormpath::Application
-        auth_result = application.authenticate_account ::UsernamePasswordRequest.new(login, password)
-        auth_result.get_account
+      def self.authenticate_account(username, password)
+        application = self.ds.get_resource ENV["STORMPATH_APPLICATION_URL"], Stormpath::Application
+        auth_result = application.authenticate_account ::UsernamePasswordRequest.new(username, password)
+        auth_result.account
       end
 
-      def self.send_password_reset_email(login_or_email)
-        application = self.ds.get_resource Config[:stormpath_url], Stormpath::Application
-        application.send_password_reset_email login_or_email
+      def self.send_password_reset_email(username_or_email)
+        application = self.ds.get_resource ENV["STORMPATH_APPLICATION_URL"], Stormpath::Application
+        application.send_password_reset_email username_or_email
       end
 
       def self.verify_password_reset_token(token)
-        application = self.ds.get_resource Config[:stormpath_url], Stormpath::Application
+        application = self.ds.get_resource ENV["STORMPATH_APPLICATION_URL"], Stormpath::Application
         application.verify_password_reset_token token
       end
 
@@ -35,7 +35,7 @@ module Stormpath
       end
 
       def self.all_accounts
-        self.root_directory.get_accounts
+        self.root_directory.accounts
       end
 
       def self.find_account(href)
@@ -53,7 +53,7 @@ module Stormpath
       end
 
       def self.root_directory
-        self.ds.get_resource Config[:root], Stormpath::Directory
+        self.ds.get_resource ENV["STORMPATH_DIRECTORY_URL"], Stormpath::Directory
       end
 
       def self.ds
@@ -61,10 +61,25 @@ module Stormpath
       end
 
       def self.client
-        self.connection ||= Stormpath::Client.new({
-          application_href: Config[:stormpath_url] || Config[:href]
-        })
-        self.connection.client
+        options = Hash.new.tap do |o|
+          set_if_not_empty(o, "application_url", ENV["STORMPATH_APPLICATION_URL"])
+          set_if_not_empty(o, "api_key_file_location", ENV["STORMPATH_API_KEY_FILE_LOCATION"])
+          set_if_not_empty(o, "api_key_id_property_name", ENV["STORMPATH_API_KEY_ID_PROPERTY_NAME"])
+          set_if_not_empty(o, "api_key_secret_property_name", ENV["STORMPATH_API_KEY_SECRET_PROPERTY_NAME"])
+
+          o[:api_key] = {
+            id: ENV["STORMPATH_API_KEY_ID"],
+            secret: ENV["STORMPATH_API_KEY_SECRET"]
+          } unless ENV["STORMPATH_API_KEY_ID"].blank? or ENV["STORMPATH_API_KEY_SECRET"].blank?
+        end
+
+        self.connection ||= Stormpath::Client.new options
+      end
+
+      private
+
+      def self.set_if_not_empty(object, property, value)
+        object[property] = value unless value.blank?
       end
     end
   end
