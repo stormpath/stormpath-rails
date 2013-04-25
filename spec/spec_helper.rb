b3b0088e8b52faf456f870862db511c49eb0a8d5
Erin Swenson-Healey <erin@carbonfive.com>
2013-04-25 15:13:55 -0700

    Adding client spec + helper for teardown/setup

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,9 +4,55 @@
 require 'simplecov'
 SimpleCov.start
 
+require "stormpath-rails"
+
 Dir["./spec/support/**/*.rb"].sort.each {|f| require f}
+
+module Stormpath
+  module TestResourceHelpers
+    def generate_resource_name
+      "Test#{SecureRandom.uuid}"
+    end
+
+    def destroy_all_stormpath_test_accounts
+      Stormpath::Rails::Client.root_directory.accounts.each do |account|
+        Stormpath::Rails::Client.delete_account! account.href
+      end
+    end
+
+    def create_test_user(opts={})
+      opts.tap do |o|
+        o["surname"]    = (!opts["surname"].blank? && opts["surname"]) || generate_resource_name
+        o["given_name"] = (!opts["given_name"].blank? && opts["given_name"]) || generate_resource_name
+        o["username"]   = (!opts["username"].blank? && opts["username"]) || generate_resource_name
+        o["password"]   = (!opts["password"].blank? && opts["password"]) || generate_resource_name
+        o["email"]      = (!opts["email"].blank? && opts["email"]) || "#{generate_resource_name}@example.com"
+      end
+
+      Stormpath::Rails::Client.create_account! opts
+    end
+  end
+end
 
 RSpec.configure do |config|
   config.mock_framework = :rspec
   config.order = 'random'
+  config.include Stormpath::TestResourceHelpers
+
+  config.before(:all) do
+    if ENV["STORMPATH_RAILS_TEST_API_KEY_SECRET"].blank? or ENV["STORMPATH_RAILS_TEST_API_KEY_ID"].blank? or ENV["STORMPATH_RAILS_TEST_APPLICATION_URL"].blank? or ENV["STORMPATH_RAILS_TEST_DIRECTORY_URL"].blank?
+      raise "consult spec_helper for required environment variables before running specs"
+    end
+
+    ENV["STORMPATH_API_KEY_SECRET"]  = ENV["STORMPATH_RAILS_TEST_API_KEY_SECRET"]
+    ENV["STORMPATH_API_KEY_ID"]      = ENV["STORMPATH_RAILS_TEST_API_KEY_ID"]
+    ENV["STORMPATH_APPLICATION_URL"] = ENV["STORMPATH_RAILS_TEST_APPLICATION_URL"]
+    ENV["STORMPATH_DIRECTORY_URL"]   = ENV["STORMPATH_RAILS_TEST_DIRECTORY_URL"]
+
+    destroy_all_stormpath_test_accounts
+  end
+
+  config.after(:all) do
+    destroy_all_stormpath_test_accounts
+  end
 end
