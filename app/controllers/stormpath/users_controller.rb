69f7d1a5559635346138bdb1a3e22047b671f66b
Nenad Nikolic <nenad.nikolic@infinum.hr>
2015-06-16 10:13:17 +0200

    adding user object to users contorller. changes in templates extracting form fields to partial

--- app/controllers/stormpath/users_controller.rb
+++ app/controllers/stormpath/users_controller.rb
@@ -1,5 +1,24 @@
 class Stormpath::UsersController < Stormpath::BaseController
+  def create
+    @user = User.new(user_params)
+    binding.pry
+
+    if @user.save
+      sign_in @user
+      redirect_to root_path
+    else
+      render template: "users/new"
+    end
+  end
+
   def new
+    @user = User.new
     render template: "users/new"
+  end
+
+  private
+
+  def user_params
+    params[:user] || Hash.new
   end
 end
