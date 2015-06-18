74b5f85e13b9135aaf7a58a3fe49054321ef76b9
Nenad Nikolic <nenad.nikolic@infinum.hr>
2015-06-18 15:34:15 +0200

    adding find_user method to user which will be used on authentication

--- lib/stormpath/rails/user.rb
+++ lib/stormpath/rails/user.rb
@@ -1,6 +1,23 @@
 module Stormpath
   module Rails
     module User
+      extend ActiveSupport::Concern
+
+      module ClassMethods
+        def find_user(email)
+          if user = find_by_normalized_email(email)
+            return user
+          end
+        end
+
+        def find_by_normalized_email(email)
+          find_by_email normalize_email(email)
+        end
+
+        def normalize_email(email)
+          email.to_s.downcase.gsub(/\s+/, "")
+        end
+      end
     end
   end
 end
