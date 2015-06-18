326d42e4061276f3ed29f890438beb4077c28496
Nenad Nikolic <nenad.nikolic@infinum.hr>
2015-06-18 11:51:21 +0200

    user sign in feature initial commit. adding route and controller/view for the form

--- app/controllers/stormpath/sessions_controller.rb
+++ app/controllers/stormpath/sessions_controller.rb
@@ -1,2 +1,5 @@
 class Stormpath::SessionsController < Stormpath::BaseController
+  def new
+    render template: "sessions/new"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resource :session, controller: 'clearance/sessions', only: [:create]
   resource :users, controller: 'stormpath/users', only: :create
   get '/sign_up' => 'stormpath/users#new', as: 'sign_up'
+  get '/sign_in' => 'stormpath/sessions#new', as: 'sign_in'
 end
