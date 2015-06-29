fc77e2544332f325e6e7e9eed04c6cc90555f06b
Nenad Nikolic <nenad.nikolic@infinum.hr>
2015-06-29 21:48:37 +0200

    changing routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resource :session, controller: 'stormpath/sessions', only: [:create]
   resource :users, controller: 'stormpath/users', only: :create
-  get '/sign_up' => 'stormpath/users#new', as: 'sign_up'
-  get '/sign_in' => 'stormpath/sessions#new', as: 'sign_in'
+  get '/register' => 'stormpath/users#new', as: 'sign_up'
+  get '/login' => 'stormpath/sessions#new', as: 'sign_in'
   delete '/sign_out' => 'stormpath/sessions#destroy', as: 'sign_out'
 end
