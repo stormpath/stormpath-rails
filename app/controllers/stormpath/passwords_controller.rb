f364bb11fc15abe2d97a9d94b5e5fba20214a7c9
Nenad Nikolic <nenad.nikolic@infinum.hr>
2015-08-03 10:34:14 +0200

    adding support for reset password feature

--- app/controllers/stormpath/passwords_controller.rb
+++ app/controllers/stormpath/passwords_controller.rb
@@ -1,2 +1,13 @@
 class Stormpath::PasswordsController < Stormpath::BaseController
+  def create
+    if find_user_by_email(params[:password][:email])
+      reset_password(params[:password][:email])
+      render template: 'passwords/new'
+    end
+    render template: 'passwords/new'
+  end
+
+  def new
+    render template: 'passwords/new'
+  end
 end
