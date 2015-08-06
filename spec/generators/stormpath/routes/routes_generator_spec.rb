467649b4c0beb6460e7120b566b8996580e65304
Nenad Nikolic <nenad.nikolic@infinum.hr>
2015-08-06 12:37:45 +0200

    adding route generator spec

---
+++ spec/generators/stormpath/routes/routes_generator_spec.rb
@@ -0,0 +1,17 @@
+require "spec_helper"
+require "generators/stormpath/routes/routes_generator"
+
+describe Stormpath::Generators::RoutesGenerator, :generator do
+  it "adds stormpath routes to existing rails routes" do
+    provide_existing_routes_file
+
+    routes = file('config/routes.rb')
+
+    run_generator
+
+    expect(routes).to have_correct_syntax
+    expect(routes).to contain(
+      "get '/login' => 'stormpath/sessions#new', as: 'sign_in'"
+    )
+  end
+end
