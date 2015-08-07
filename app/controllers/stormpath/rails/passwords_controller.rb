74e9a547ae0ff443904d0bdb46999c9c5b9b5f06
Nenad Nikolic <nenad.nikolic@infinum.hr>
2015-08-07 14:39:37 +0200

    adding confirmation view for reset password

--- app/controllers/stormpath/rails/passwords_controller.rb
+++ app/controllers/stormpath/rails/passwords_controller.rb
@@ -2,9 +2,10 @@ class Stormpath::Rails::PasswordsController < Stormpath::Rails::BaseController
   def create
     if find_user_by_email(params[:password][:email])
       reset_password(params[:password][:email])
+      render template: 'passwords/create'
+    else
       render template: 'passwords/new'
     end
-    render template: 'passwords/new'
   end
 
   def new
