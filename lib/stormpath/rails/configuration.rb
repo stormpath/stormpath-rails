f6ebe2e2dbd5d25f8f7d690ef03b306112732502
Nenad Nikolic <nenad.nikolic@infinum.hr>
2015-08-12 11:52:53 +0200

    adding verify email option to configuration

--- lib/stormpath/rails/configuration.rb
+++ lib/stormpath/rails/configuration.rb
@@ -1,11 +1,12 @@
 module Stormpath
   module Rails
     class Configuration
-      attr_accessor :api_key_file, :secret_key, :application, :expand_custom_data, :id_site, :user_model
+      attr_accessor :api_key_file, :secret_key, :application, :expand_custom_data, :id_site, :user_model, :verify_email
 
       def initialize
         @id_site = false
         @expand_custom_data = true
+        @verify_email = false
       end
 
       def user_model
