8f840e10328ac0eeb6778bcba497e940ec74c6f1
Nenad Nikolic <nenad.nikolic@infinum.hr>
2015-08-18 16:37:51 +0200

    adding id site implementation on users controller

--- app/controllers/stormpath/rails/users_controller.rb
+++ app/controllers/stormpath/rails/users_controller.rb
@@ -20,8 +20,16 @@ class Stormpath::Rails::UsersController < Stormpath::Rails::BaseController
   end
 
   def new
-    @user = user_from_params
-    render template: "users/new"
+    if Stormpath::Rails.config.id_site.enabled
+      redirect_to id_site_url callback_uri: (request.base_url + Stormpath::Rails.config.id_site.uri), path: '/#register'
+    else
+      if signed_in?
+        redirect_to root_path
+      else
+        @user = user_from_params
+        render template: "users/new"
+      end
+    end
   end
 
   def verify
