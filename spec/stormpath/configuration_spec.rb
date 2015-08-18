e4b91629873c47cf1191267922b3058e26c519ba
Nenad Nikolic <nenad.nikolic@infinum.hr>
2015-08-18 16:34:41 +0200

    changes in configuration spec

--- spec/stormpath/configuration_spec.rb
+++ spec/stormpath/configuration_spec.rb
@@ -4,33 +4,29 @@ describe Stormpath::Rails::Configuration do
 
   context 'when id_site is not specified' do
     before do
-      Stormpath::Rails.configure do
+      Stormpath::Rails.configure do |config|
+        config.id_site = {}
       end
     end
 
     it 'defaults to false' do
-      expect(Stormpath::Rails.config.id_site).to eq false
+      expect(Stormpath::Rails.config.id_site.enabled).to eq false
     end
   end
 
   context 'when is_site is set to true' do
     before do
       Stormpath::Rails.configure do |config|
-        config.id_site = true
+        config.id_site = { enabled: true }
       end
     end
 
     it 'returns true' do
-      expect(Stormpath::Rails.config.id_site).to eq true
+      expect(Stormpath::Rails.config.id_site.enabled).to eq true
     end
   end
 
   context 'when expand_custom_data is not specified' do
-    before do
-      Stormpath::Rails.configure do
-      end
-    end
-
     it 'defaults to false' do
       expect(Stormpath::Rails.config.expand_custom_data).to eq true
     end
