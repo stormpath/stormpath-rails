570673283742517ede24565786d80464168889f2
Nenad Nikolic <nenad.nikolic@infinum.hr>
2015-08-24 14:38:51 +0200

    changing the configuration system in configuration class and make sure the tests pass

--- lib/stormpath/rails/configuration.rb
+++ lib/stormpath/rails/configuration.rb
@@ -15,11 +15,43 @@ module Stormpath
       end
 
       def id_site=(options)
-        @id_site = IdSite.new(options)
+        @id_site = UserConfig::IdSite.new(options)
       end
 
       def id_site
-        @id_site ||= IdSite.new
+        @id_site ||= UserConfig::IdSite.new
+      end
+
+      def api_key=(options)
+        @api_key = UserConfig::ApiKey.new(options)
+      end
+
+      def api_key
+        @api_key ||= UserConfig::ApiKey.new
+      end
+
+      def application=(options)
+        @application = UserConfig::Application.new(options)
+      end
+
+      def application
+        @application ||= UserConfig::Application.new
+      end
+
+      def verify_email=(options)
+        @verify_email = UserConfig::VerifyEmail.new(options)
+      end
+
+      def verify_email
+        @verify_email ||= UserConfig::VerifyEmail.new
+      end
+
+      def forgot_password=(options)
+        @forgot_password = UserConfig::ForgotPassword.new(options)
+      end
+
+      def forgot_password
+        @forgot_password ||= UserConfig::ForgotPassword.new
       end
     end
 
@@ -31,8 +63,12 @@ module Stormpath
 
     # Configure the settings for this module
     # @param [lambda] which will be passed isntance of configuration class
-    def self.configure
-      yield config
+    def self.configure(config_data)
+      config.id_site = config_data[:web][:id_site] if config_data[:web] && config_data[:web][:id_site]
+      config.api_key = config_data[:client][:api_key] if config_data[:client] && config_data[:client][:api_key]
+      config.application = config_data[:application] if config_data[:application]
+      config.verify_email = config_data[:web][:verify_email] if config_data[:web] && config_data[:web][:verify_email]
+      config.forgot_password = config_data[:web][:forgot_password] if config_data[:web] && config_data[:web][:forgot_password]
     end
   end
 end
