b68e83ec24acea4ceb052c261784607687f40dce
Nenad Nikolic <nenad.nikolic@infinum.hr>
2015-08-25 15:05:49 +0200

    adding additional tests for install generator

--- spec/generators/stormpath/install/install_generator_spec.rb
+++ spec/generators/stormpath/install/install_generator_spec.rb
@@ -1,7 +1,7 @@
 require "spec_helper"
 require "generators/stormpath/install/install_generator"
 
-describe Stormpath::Generators::InstallGenerator, :generator do
+describe Stormpath::Generators::InstallGenerator, type: :generator do
   describe "initializer" do
     it "is copied to the application" do
       provide_existing_application_controller
@@ -38,6 +38,39 @@ describe Stormpath::Generators::InstallGenerator, :generator do
         expect(user_class).to exist
         expect(user_class).to have_correct_syntax
         expect(user_class).to contain("Stormpath::Rails::User")
+      end
+    end
+  end
+
+  context "user class already exists" do
+    it "includes Clearance::User" do
+      provide_existing_application_controller
+      provide_existing_user_class
+      #binding.pry
+
+      run_generator
+      user_class = file("app/models/user.rb")
+
+      expect(user_class).to exist
+      expect(user_class).to have_correct_syntax
+      expect(user_class).to contain("include Stormpath::Rails::User")
+    end
+  end
+
+  describe "user migration" do
+    context "users table does not exist" do
+      it "creates a migration to create the users table" do
+        provide_existing_application_controller
+        allow(ActiveRecord::Base.connection).to receive(:table_exists?).
+          with(:users).
+          and_return(false)
+
+        run_generator
+        migration = migration_file("db/migrate/create_users.rb")
+
+        expect(migration).to exist
+        expect(migration).to have_correct_syntax
+        expect(migration).to contain("create_table :users")
       end
     end
   end
