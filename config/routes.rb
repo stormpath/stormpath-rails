d631bba41d81647bd118752e72ad7b9005e063f6
Nenad Nikolic <nenad.nikolic@infinum.hr>
2015-08-26 10:38:02 +0200

    grouping routs in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
   resource :session, controller: 'stormpath/rails/sessions', only: [:create]
   resource :users, controller: 'stormpath/rails/users', only: :create
-  get '/register' => 'stormpath/rails/users#new', as: 'sign_up'
-  get '/login' => 'stormpath/rails/sessions#new', as: 'sign_in'
+  get    '/register' => 'stormpath/rails/users#new', as: 'sign_up'
+  get    '/verify'   => 'stormpath/rails/users#verify', as: 'verify'
+
+  get    '/login'    => 'stormpath/rails/sessions#new', as: 'sign_in'
   delete '/sign_out' => 'stormpath/rails/sessions#destroy', as: 'sign_out'
-  get '/forgot' => 'stormpath/rails/passwords#forgot', as: 'forgot'
-  post '/forgot' => 'stormpath/rails/passwords#forgot_send', as: 'forgot_send'
-  get '/forgot/change' => 'stormpath/rails/passwords#forgot_change', as: 'forgot_change'
-  post '/forgot/change/:account_url' => 'stormpath/rails/passwords#forgot_update', as: 'forgot_update'
-  get '/verify' => 'stormpath/rails/users#verify', as: 'verify'
-  get '/redirect' => 'stormpath/rails/sessions#redirect', as: 'redirect'
+  get    '/redirect' => 'stormpath/rails/sessions#redirect', as: 'redirect'
+
+  get    '/forgot'   => 'stormpath/rails/passwords#forgot', as: 'forgot'
+  post   '/forgot'   => 'stormpath/rails/passwords#forgot_send', as: 'forgot_send'
+  get    '/forgot/change' => 'stormpath/rails/passwords#forgot_change', as: 'forgot_change'
+  post   '/forgot/change/:account_url' => 'stormpath/rails/passwords#forgot_update', as: 'forgot_update'
 end
