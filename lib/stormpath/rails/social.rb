4703809229e82f602fe7cb4587277ad22b9475ae
Nenad Nikolic <nenad.nikolic@infinum.hr>
2015-08-31 14:44:43 +0200

    adding spec for social helper, moving facebook_app_id to controller/view helper method

--- lib/stormpath/rails/social.rb
+++ lib/stormpath/rails/social.rb
@@ -4,14 +4,19 @@ module Stormpath
       extend ActiveSupport::Concern
 
       included do
-        helper_method :facebook_login_enabled?
+        helper_method :facebook_login_enabled?, :facebook_app_id
         hide_action(
-          :facebook_login_enabled?
+          :facebook_login_enabled?,
+          :facebook_app_id
         )
       end
 
       def facebook_login_enabled?
         configuration.facebook.enabled? 
+      end
+
+      def facebook_app_id
+        configuration.facebook.app_id
       end
     end
   end
