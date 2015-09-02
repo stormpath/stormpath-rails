f322e4f557fa3d3979cd278d2f2a648e530687c4
Nenad Nikolic <nenad.nikolic@infinum.hr>
2015-09-02 12:07:13 +0200

    removing base class for configuration objects since virtus already accepts block although it isnt in documentation

--- lib/stormpath/rails/user_config/id_site.rb
+++ lib/stormpath/rails/user_config/id_site.rb
@@ -1,7 +1,9 @@
 module Stormpath
   module Rails
     module UserConfig
-      class IdSite < Base
+      class IdSite
+        include Virtus.model
+
         attribute :enabled, Boolean, default: false
         attribute :uri, String
         attribute :login_uri, String
