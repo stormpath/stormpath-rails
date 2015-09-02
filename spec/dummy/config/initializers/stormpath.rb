ffa4f8761d80fdd506e82f9b06103742950224a7
Nenad Nikolic <nenad.nikolic@infinum.hr>
2015-09-02 12:11:00 +0200

    changing dummy app config for tests

--- spec/dummy/config/initializers/stormpath.rb
+++ spec/dummy/config/initializers/stormpath.rb
@@ -1,17 +1,15 @@
-# Stormpath::Rails.configure do |config|
-#   config.api_key_file = ENV['STORMPATH_API_KEY_FILE_LOCATION']
-#   config.application = ENV['STORMPATH_SDK_TEST_APPLICATION_URL']
-# end
-
-
-binding.pry
 Stormpath::Rails.configure do |config|
+  binding.pry
   config.id_site do |c|
     c.enabled = false
     c.uri = "/idSiteResult"
   end
 
-  config.api_key do |c|
-    c.file = ENV['STORMPATH_API_KEY_FILE_LOCATION']
+  config.api_key.file = ENV['STORMPATH_API_KEY_FILE_LOCATION']
+  config.application.href = ENV['STORMPATH_SDK_TEST_APPLICATION_URL']
+
+  config.facebook do |c|
+    c.app_id = '913427355397270'
+    c.app_secret = 'eddfb07802b3f3984989696bfb70a0ee'
   end
 end
