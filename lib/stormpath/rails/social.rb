8129263f5c051648b8271abe6907627baf789b2b
Nenad Nikolic <nenad.nikolic@infinum.hr>
2015-09-08 12:31:22 +0200

    adding helpers for google login and social auth

--- lib/stormpath/rails/social.rb
+++ lib/stormpath/rails/social.rb
@@ -4,10 +4,13 @@ module Stormpath
       extend ActiveSupport::Concern
 
       included do
-        helper_method :facebook_login_enabled?, :facebook_app_id
+        helper_method :facebook_login_enabled?, :facebook_app_id,
+          :google_login_enabled?, :google_client_id, :social_auth?
         hide_action(
           :facebook_login_enabled?,
-          :facebook_app_id
+          :google_login_enabled?,
+          :facebook_app_id,
+          :google_client_id
         )
       end
 
@@ -15,8 +18,20 @@ module Stormpath
         configuration.facebook.enabled? 
       end
 
+      def google_login_enabled?
+        configuration.google.enabled?
+      end
+
       def facebook_app_id
         configuration.facebook.app_id
+      end
+
+      def google_client_id
+        configuration.google.client_id
+      end
+
+      def social_auth?
+        facebook_login_enabled? || google_login_enabled?
       end
     end
   end
