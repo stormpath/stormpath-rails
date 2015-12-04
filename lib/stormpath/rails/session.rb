6d39958b83c2ab4b1dc7f4e2304c185141c07938
Nenad Nikolic <nenad.nikolic@infinum.hr>
2015-12-04 12:49:02 +0100

    adding user href to session initializer

--- lib/stormpath/rails/session.rb
+++ lib/stormpath/rails/session.rb
@@ -9,12 +9,14 @@ module Stormpath
 
       private
 
-      def initialize_session(user)
+      def initialize_session(user, href)
         session[:user_id] = user.id
+        session[:user_href] = href
       end
 
       def reset_session
         session[:user_id] = nil
+        session[:user_href] = nil
       end
 
       def logout
@@ -31,6 +33,10 @@ module Stormpath
 
       def current_user
         @current_user ||= configuration.user_model.find(session[:user_id]) if session[:user_id]
+      end
+
+      def current_user_href
+        session[:user_href] if session[:user_href]
       end
     end
   end
