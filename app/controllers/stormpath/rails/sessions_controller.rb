6223b8213b2054ea374cc5df3becee29c3a4ebfc
Nenad Nikolic <nenad.nikolic@infinum.hr>
2015-12-08 11:38:45 +0100

    removing unnecesary code

--- app/controllers/stormpath/rails/sessions_controller.rb
+++ app/controllers/stormpath/rails/sessions_controller.rb
@@ -25,12 +25,6 @@ class Stormpath::Rails::SessionsController < Stormpath::Rails::BaseController
 
   def destroy
     logout
-    set_flash_message :notice, 'You have been logged out successfully.'
-    redirect_to configuration.logout.next_uri
-  end
-  
-  def destroy
-    logout
 
     respond_to do |format|
       format.json { render nothing: true, status: 200 }
