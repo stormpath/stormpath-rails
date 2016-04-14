c9ba43d7ffbe2cd51bdd24c3dd93213a9972b6cd
Damir Svrtan <damir.svrtan@gmail.com>
2016-04-14 23:34:08 +0200

    Add constants to ContentTypeNegotiator

--- lib/stormpath/rails/content_type_negotiator.rb
+++ lib/stormpath/rails/content_type_negotiator.rb
@@ -3,13 +3,17 @@ module Stormpath
     class ContentTypeNegotiator
       attr_accessor :accept_header
 
+      ACCEPT_WILDCARD = '*/*'
+      HTTP_ACCEPT_JSON = 'application/json'
+      HTTP_ACCEPT_HTML = 'text/html'
+
       def initialize(accept_header)
-        accept_header = accept_header.presence || '*/*'
+        accept_header = accept_header.presence || ACCEPT_WILDCARD
         @accept_header = normalize(accept_header)
       end
 
       def call
-        if accept_header == "*/*"
+        if accept_header == ACCEPT_WILDCARD
           Stormpath::Rails.config.produces.accepts.first
         elsif accept_header.in?(Stormpath::Rails.config.produces.accepts)
           accept_header
@@ -19,10 +23,10 @@ module Stormpath
       def normalize(accept_header)
         return accept_header unless accept_header.include?(',')
 
-        if accept_header.include?('application/json')
-          'application/json'
-        elsif accept_header.include?('html/text')
-          'html/text'
+        if accept_header.include?(HTTP_ACCEPT_JSON)
+          HTTP_ACCEPT_JSON
+        elsif accept_header.include?(HTTP_ACCEPT_HTML)
+          HTTP_ACCEPT_HTML
         end
       end
     end
