b3456c09ef9aa8e44cd4a0d03a6a416f3a991388
Damir Svrtan <damir.svrtan@gmail.com>
2016-04-19 12:00:00 +0200

    Add account serializer

--- app/controllers/stormpath/rails/sessions_controller.rb
+++ app/controllers/stormpath/rails/sessions_controller.rb
@@ -1,121 +1,108 @@
-class Stormpath::Rails::SessionsController < Stormpath::Rails::BaseController
-  before_action :redirect_signed_in_users, only: :new
-  before_action :inspect_for_missing_fields, only: :create
+module Stormpath
+  module Rails
+    class SessionsController < BaseController
+      before_action :redirect_signed_in_users, only: :new
+      before_action :inspect_for_missing_fields, only: :create
 
-  def create
-    result = authenticate user_from_params
+      def create
+        result = authenticate user_from_params
 
-    if result.success?
-      @user = find_or_create_user_from_account result.account
-      initialize_session(@user, result.account.href)
+        if result.success?
+          @user = find_or_create_user_from_account result.account
+          initialize_session(@user, result.account.href)
 
-      respond_to do |format|
-        format.json { render json: login_serializer(result.account) }
-        format.html { redirect_to login_redirect_route, notice: 'Successfully signed in' }
-      end
-    else
-      respond_to do |format|
-        format.json do
-          status = result.status.presence || 400
-          render json: { status: status, message: result.error_message }, status: status
+          respond_to do |format|
+            format.json { render json: AccountSerializer.to_h(result.account) }
+            format.html { redirect_to login_redirect_route, notice: 'Successfully signed in' }
+          end
+        else
+          respond_to do |format|
+            format.json do
+              status = result.status.presence || 400
+              render json: { status: status, message: result.error_message }, status: status
+            end
+            format.html do
+              set_flash_message :error, result.error_message
+              render template: "sessions/new"
+            end
+          end
         end
-        format.html do
-          set_flash_message :error, result.error_message
+      end
+
+      def destroy
+        logout
+
+        respond_to do |format|
+          format.json { render nothing: true, status: 200 }
+          format.html do
+            set_flash_message :notice, 'You have been logged out successfully.'
+            redirect_to configuration.logout.next_uri
+          end
+        end
+      end
+
+      def new
+        if !configuration.login.enabled?
+          redirect_to configuration.login.next_uri
+        elsif configuration.id_site.enabled?
+          redirect_to id_site_login_url
+        else
           render template: "sessions/new"
         end
       end
-    end
-  end
 
-  def destroy
-    logout
+      def redirect
+        user_data = handle_id_site_callback(request.url)
+        @user = find_user_by_email user_data.email
+        initialize_session(@user, user_data.href)
 
-    respond_to do |format|
-      format.json { render nothing: true, status: 200 }
-      format.html do
-        set_flash_message :notice, 'You have been logged out successfully.'
-        redirect_to configuration.logout.next_uri
+        redirect_to configuration.id_site.next_uri, notice: 'Successfully signed in'
       end
-    end
-  end
 
-  def new
-    if !configuration.login.enabled?
-      redirect_to configuration.login.next_uri
-    elsif configuration.id_site.enabled?
-      redirect_to id_site_login_url
-    else
-      render template: "sessions/new"
-    end
-  end
+      private
 
-  def redirect
-    user_data = handle_id_site_callback(request.url)
-    @user = find_user_by_email user_data.email
-    initialize_session(@user, user_data.href)
+      def user_from_params
+        username = params[:login]
+        password = params[:password]
 
-    redirect_to configuration.id_site.next_uri, notice: 'Successfully signed in'
-  end
+        ::User.new.tap do |user|
+          user.email = username
+          user.password = password
+        end
+      end
 
-  private
+      def redirect_signed_in_users
+        redirect_to root_path if signed_in?
+      end
 
-  def user_from_params
-    username = params[:login]
-    password = params[:password]
+      def login_redirect_route
+        if params[:next]
+          login_redirect_route_from_params
+        else
+          configuration.login.next_uri
+        end
+      end
 
-    ::User.new.tap do |user|
-      user.email = username
-      user.password = password
-    end
-  end
+      def login_redirect_route_from_params
+        if params[:next].start_with?('/')
+          params[:next]
+        else
+          "/#{params[:next]}"
+        end
+      end
 
-  def redirect_signed_in_users
-    redirect_to root_path if signed_in?
-  end
-
-  def login_redirect_route
-    if params[:next]
-      login_redirect_route_from_params
-    else
-      configuration.login.next_uri
-    end
-  end
-
-  def login_redirect_route_from_params
-    if params[:next].start_with?('/')
-      params[:next]
-    else
-      "/#{params[:next]}"
-    end
-  end
-
-  def login_serializer(account)
-    {
-      account: {
-        href: account.href,
-        username: account.username,
-        modifiedAt: nil, #account.modified_at,
-        status: account.status,
-        createdAt: nil, #account.created_at,
-        email: account.email,
-        middleName: account.middle_name,
-        surname: account.surname,
-        givenName: account.given_name,
-        fullName: account.full_name
-      }
-    }
-  end
-
-  def inspect_for_missing_fields
-    if params[:login].blank? && params[:password].blank?
-      flash[:error] = "Login and password fields can't be blank"
-      render template: "sessions/new"
-    elsif params[:login].blank?
-      flash[:error] = "Login field can't be blank"
-      render template: "sessions/new"
-    elsif params[:password].blank?
-      flash[:error] = "Password field can't be blank"
-      render template: "sessions/new"
+      def inspect_for_missing_fields
+        if params[:login].blank? && params[:password].blank?
+          flash[:error] = "Login and password fields can't be blank"
+          render template: "sessions/new"
+        elsif params[:login].blank?
+          flash[:error] = "Login field can't be blank"
+          render template: "sessions/new"
+        elsif params[:password].blank?
+          flash[:error] = "Password field can't be blank"
+          render template: "sessions/new"
+        end
+      end
     end
   end
 end
---
+++ app/serializers/stormpath/rails/account_serializer.rb
@@ -0,0 +1,32 @@
+module Stormpath
+  module Rails
+    class AccountSerializer
+      attr_reader :account
+
+      def self.to_h(account)
+        new(account).to_h
+      end
+
+      def initialize(account)
+        @account = account
+      end
+
+      def to_h
+        {
+          account: {
+            href: account.href,
+            username: account.username,
+            modifiedAt: nil, #account.modified_at,
+            status: account.status,
+            createdAt: nil, #account.created_at,
+            email: account.email,
+            middleName: account.middle_name,
+            surname: account.surname,
+            givenName: account.given_name,
+            fullName: account.full_name
+          }
+        }
+      end
+    end
+  end
+end
