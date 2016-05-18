6e5faa0ea9eb4c191f782bd1d8d1da6e2d9d7910
Damir Svrtan <damir.svrtan@gmail.com>
2016-05-18 15:56:36 +0200

    Delete user config in Ruby

--- lib/stormpath/rails.rb
+++ lib/stormpath/rails.rb
@@ -20,22 +20,5 @@ module Stormpath
     autoload :Social, 'stormpath/rails/social'
     autoload :ContentTypeNegotiator, 'stormpath/rails/content_type_negotiator'
     autoload :RoutingConstraint, 'stormpath/rails/routing_constraint'
-
-    module UserConfig
-      autoload :AccessTokenCookie, 'stormpath/rails/user_config/access_token_cookie'
-      autoload :ApiKey, 'stormpath/rails/user_config/api_key'
-      autoload :Application, 'stormpath/rails/user_config/application'
-      autoload :ChangePassword, 'stormpath/rails/user_config/change_password'
-      autoload :ForgotPassword, 'stormpath/rails/user_config/forgot_password'
-      autoload :IdSite, 'stormpath/rails/user_config/id_site'
-      autoload :VerifyEmail, 'stormpath/rails/user_config/verify_email'
-      autoload :Facebook, 'stormpath/rails/user_config/facebook'
-      autoload :Google, 'stormpath/rails/user_config/google'
-      autoload :Login, 'stormpath/rails/user_config/login'
-      autoload :Logout, 'stormpath/rails/user_config/logout'
-      autoload :RefreshTokenCookie, 'stormpath/rails/user_config/refresh_token_cookie'
-      autoload :Register, 'stormpath/rails/user_config/register'
-      autoload :Produces, 'stormpath/rails/user_config/produces'
-    end
   end
 end
