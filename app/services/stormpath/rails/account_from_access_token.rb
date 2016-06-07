7fe957b5d41a39e7ec24434e0d6dfb1241bfd7dc
Damir Svrtan <damir.svrtan@gmail.com>
2016-06-07 11:16:24 +0200

    Add non-tested account from access token service

---
+++ app/services/stormpath/rails/account_from_access_token.rb
@@ -0,0 +1,29 @@
+module Stormpath
+  module Rails
+    class AccountFromAccessToken
+      attr_reader :access_token
+
+      def initialize(access_token)
+        @access_token = access_token
+      end
+
+      def account
+        @account ||= Stormpath::Rails::Client.client.accounts.get(account_href)
+      end
+
+      private
+
+      def account_href
+        jwt_response['sub']
+      end
+
+      def jwt_response
+        begin
+          JWT.decode(access_token, ENV['STORMPATH_API_KEY_SECRET']).first
+        rescue JWT::ExpiredSignature
+          raise Stormpath::Oauth::Error.new(:jwt_expired)
+        end
+      end
+    end
+  end
+end
