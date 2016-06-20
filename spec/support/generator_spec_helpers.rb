31229657be9104a1ca1ba33ff0a0624f2d79b820
Damir Svrtan <damir.svrtan@gmail.com>
2016-06-20 14:54:27 +0200

    Remove unused generator spec helpers

--- spec/support/generator_spec_helpers.rb
+++ spec/support/generator_spec_helpers.rb
@@ -5,18 +5,8 @@ require 'ammeter/init'
 module GeneratorSpecHelpers
   TEMPLATE_PATH = File.expand_path('../../app_templates', __FILE__)
 
-  def provide_existing_routes_file
-    copy_to_generator_root('config', 'routes.rb')
-  end
-
   def provide_existing_application_controller
     copy_to_generator_root('app/controllers', 'application_controller.rb')
-  end
-
-  def provide_existing_user_class
-    copy_to_generator_root('app/models', 'user.rb')
-    allow(File).to receive(:exist?).and_call_original
-    allow(File).to receive(:exist?).with('app/models/user.rb').and_return(true)
   end
 
   private
