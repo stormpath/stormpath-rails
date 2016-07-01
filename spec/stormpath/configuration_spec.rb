62f81c24bfc5f5a97cd758f73efcc9f0e25a5141
Damir Svrtan <damir.svrtan@gmail.com>
2016-07-01 10:38:38 +0200

    Add specs for configuration of forgot/change password and app name

--- spec/stormpath/configuration_spec.rb
+++ spec/stormpath/configuration_spec.rb
@@ -18,4 +18,72 @@ describe Stormpath::Rails::Configuration, vcr: true do
   it 'configuration loads' do # Smoke Test
     expect(configuration.web.register.form.fields.given_name.enabled).to be(true)
   end
+
+  it 'forgot password enabled by default configuration' do
+    expect(configuration.web.forgot_password.enabled).to be(true)
+  end
+
+  it 'change password enabled by default configuration' do
+    expect(configuration.web.change_password.enabled).to be(true)
+  end
+
+  describe 'set forgot password enabled to false' do
+    let(:user_defined_config_hash) do
+      {
+        stormpath: {
+          application: {
+            href: Stormpath::Rails::Client.application.href
+          },
+          web: {
+            forgot_password: {
+              enabled: false
+            }
+          }
+        }
+      }.deep_stringify_keys!
+    end
+
+    it 'should return false after dynamic configuration' do
+      expect(stormpath_rails_config.web.forgot_password.enabled).to be(false)
+    end
+  end
+
+  describe 'set change password enabled to false' do
+    let(:user_defined_config_hash) do
+      {
+        stormpath: {
+          application: {
+            href: Stormpath::Rails::Client.application.href
+          },
+          web: {
+            change_password: {
+              enabled: false
+            }
+          }
+        }
+      }.deep_stringify_keys!
+    end
+
+    it 'should return false after dynamic configuration' do
+      expect(stormpath_rails_config.web.change_password.enabled).to be(false)
+    end
+  end
+
+  describe 'app name set' do
+    let(:user_defined_config_hash) do
+      {
+        stormpath: {
+          application: {
+            name: Stormpath::Rails::Client.application.name
+          }
+        }
+      }.deep_stringify_keys!
+    end
+
+    it 'should set app href after dynamic configuration' do
+      expect(stormpath_rails_config.application.href).to be(
+        Stormpath::Rails::Client.application.href
+      )
+    end
+  end
 end
