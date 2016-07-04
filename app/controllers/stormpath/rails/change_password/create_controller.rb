d2e57978444eaef0af25cb2348d20ef9cb20a80d
Damir Svrtan <damir.svrtan@gmail.com>
2016-07-04 22:23:14 +0200

    Refactor ChangePassword::CreateController

--- app/controllers/stormpath/rails/change_password/create_controller.rb
+++ app/controllers/stormpath/rails/change_password/create_controller.rb
@@ -3,22 +3,46 @@ module Stormpath
     module ChangePassword
       class CreateController < Stormpath::Rails::BaseController
         def call
-          password_change = PasswordChange.new(params[:sptoken], params[:password])
           password_change.call
+          respond_with_success
+        rescue Stormpath::Error => error
+          respond_to_stormpath_error(error)
+        rescue InvalidSptokenError => error
+          respond_with_error(error, configuration.web.change_password.error_uri)
+        rescue NoSptokenError => error
+          respond_with_error(error, configuration.web.forgot_password.uri)
+        end
 
+        private
+
+        def password_change
+          @password_change ||= PasswordChange.new(params[:sptoken], params[:password])
+        end
+
+        def respond_with_success
           if configuration.web.change_password.auto_login
             AccountLogin.call(cookies, password_change.account.email, params[:password])
-            respond_to do |format|
-              format.html { redirect_to configuration.web.login.next_uri }
-              format.json { render json: AccountSerializer.to_h(password_change.account) }
-            end
+            respond_to_autologin
           else
-            respond_to do |format|
-              format.html { redirect_to configuration.web.change_password.next_uri }
-              format.json { render nothing: true, status: 200 }
-            end
+            respond_without_login
           end
-        rescue Stormpath::Error => error
+        end
+
+        def respond_to_autologin
+          respond_to do |format|
+            format.html { redirect_to configuration.web.login.next_uri }
+            format.json { render json: AccountSerializer.to_h(password_change.account) }
+          end
+        end
+
+        def respond_without_login
+          respond_to do |format|
+            format.html { redirect_to configuration.web.change_password.next_uri }
+            format.json { render nothing: true, status: 200 }
+          end
+        end
+
+        def respond_to_stormpath_error(error)
           respond_to do |format|
             format.html do
               flash.now[:error] = error.message
@@ -28,16 +52,11 @@ module Stormpath
               render json: { status: error.status, message: error.message }, status: error.status
             end
           end
-        rescue InvalidSptokenError => error
+        end
+
+        def respond_with_error(error, redirect_path)
           respond_to do |format|
-            format.html { redirect_to configuration.web.change_password.error_uri }
-            format.json do
-              render json: { status: error.status, message: error.message }, status: error.status
-            end
-          end
-        rescue NoSptokenError => error
-          respond_to do |format|
-            format.html { redirect_to configuration.web.forgot_password.uri }
+            format.html { redirect_to redirect_path }
             format.json do
               render json: { status: error.status, message: error.message }, status: error.status
             end
