70661acca93ac9a102eeb683f46f2a59a006b2b5
Damir Svrtan <damir.svrtan@gmail.com>
2016-07-07 14:04:39 +0200

    Separate token cookie setter

--- app/services/stormpath/rails/token_cookie_setter.rb
+++ app/services/stormpath/rails/token_cookie_setter.rb
@@ -1,7 +1,7 @@
 module Stormpath
   module Rails
     class TokenCookieSetter
-      attr_reader :cookie_jar, :access_token, :refresh_token, :token_duration
+      attr_reader :cookie_jar, :access_token, :refresh_token
 
       def self.call(cookie_jar, authentication_result)
         new(cookie_jar, authentication_result).call
@@ -11,43 +11,14 @@ module Stormpath
         @cookie_jar     = cookie_jar
         @access_token   = authentication_result.access_token
         @refresh_token  = authentication_result.refresh_token
-        @token_duration = authentication_result.expires_in
       end
 
       def call
-        cookie_jar[access_token_cookie_config.name]  = access_token_cookie_data
-        cookie_jar[refresh_token_cookie_config.name] = refresh_token_cookie_data
+        SingleTokenCookieSetter.new(cookie_jar, access_token, access_token_cookie_config).call
+        SingleTokenCookieSetter.new(cookie_jar, refresh_token, refresh_token_cookie_config).call
       end
 
       private
-
-      def access_token_cookie_data
-        {
-          value: access_token,
-          expires: access_token_expiration_datetime,
-          httponly: access_token_cookie_config.http_only,
-          path: access_token_cookie_config.domain,
-          secure: access_token_cookie_config.secure
-        }
-      end
-
-      def refresh_token_cookie_data
-        {
-          value: refresh_token,
-          expires: refresh_token_expiration_datetime,
-          httponly: refresh_token_cookie_config.http_only,
-          path: refresh_token_cookie_config.domain,
-          secure: refresh_token_cookie_config.secure
-        }
-      end
-
-      def access_token_expiration_datetime
-        Time.zone.at(JWT.decode(access_token, ENV['STORMPATH_API_KEY_SECRET']).first['exp'])
-      end
-
-      def refresh_token_expiration_datetime
-        Time.zone.at(JWT.decode(refresh_token, ENV['STORMPATH_API_KEY_SECRET']).first['exp'])
-      end
 
       def access_token_cookie_config
         Stormpath::Rails.config.web.access_token_cookie
@@ -57,5 +28,35 @@ module Stormpath
         Stormpath::Rails.config.web.refresh_token_cookie
       end
     end
+  end
+end
+
+class SingleTokenCookieSetter
+  attr_reader :cookie_jar, :token, :token_config
+
+  def initialize(cookie_jar, token, token_config)
+    @cookie_jar = cookie_jar
+    @token = token
+    @token_config = token_config
+  end
+
+  def call
+    cookie_jar[token_config.name] = cookie_data
+  end
+
+  private
+
+  def cookie_data
+    {
+      value: token,
+      expires: token_expiration_datetime,
+      httponly: token_config.http_only,
+      path: token_config.domain,
+      secure: token_config.secure
+    }
+  end
+
+  def token_expiration_datetime
+    Time.zone.at(JWT.decode(token, ENV['STORMPATH_API_KEY_SECRET']).first['exp'])
   end
 end
