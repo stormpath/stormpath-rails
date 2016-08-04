9396d18a5534256ae5f76dd2927e4b0033ccd341
Damir Svrtan <damir.svrtan@gmail.com>
2016-08-04 15:06:21 +0200

    Dont use assets if Rails 5 API mode

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,1 +1,3 @@
-Rails.application.config.assets.precompile += %w( stormpath.css )
+if Rails.application.config.respond_to?(:assets)
+  Rails.application.config.assets.precompile += %w(stormpath.css)
+end
