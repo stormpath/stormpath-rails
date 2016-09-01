a90c3e474af1362bc987b039aaf5817564fb51fd
Marko Ćilimković <marko.cilimkovic@infinum.hr>
2016-09-01 13:35:48 +0200

    Add specs for social login verification class

--- spec/stormpath/social_login_verification_spec.rb
+++ spec/stormpath/social_login_verification_spec.rb
@@ -2,13 +2,27 @@ require 'spec_helper'
 
 describe Stormpath::Rails::Config::SocialLoginVerification, vcr: true do
   let!(:social_login_verification_class) { Stormpath::Rails::Config::SocialLoginVerification }
-  let!(:social_login_verification) { social_login_verification_class.new(app_href, true) }
+  let!(:verification) { social_login_verification_class.new(app_href, true) }
   let!(:app_href) { Stormpath::Rails::Client.application.href }
   let!(:app_name) { Stormpath::Rails::Client.application.name }
 
   describe 'with directories for social login set' do
-    it 'should retrieve facebook directory' do
-      expect(social_login_verification.facebook).to_not be nil
+    describe 'directories should contain providers' do
+      it 'should retrieve facebook provider' do
+        expect(verification.facebook.provider.provider_id).to eq 'facebook'
+      end
+
+      it 'should retrieve google provider' do
+        expect(verification.google.provider.provider_id).to eq 'google'
+      end
+
+      it 'should retrieve github provider' do
+        expect(verification.github.provider.provider_id).to eq 'github'
+      end
+
+      it 'should retrieve linkedin provider' do
+        expect(verification.linkedin.provider.provider_id).to eq 'linkedin'
+      end
     end
   end
 end
