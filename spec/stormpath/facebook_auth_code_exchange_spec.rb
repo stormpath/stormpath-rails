ffa8df7cf561f301fc8a73a67f652ca34308389c
Marko Ćilimković <marko.cilimkovic@infinum.hr>
2016-09-06 12:08:37 +0200

    Fix failing specs

--- spec/stormpath/facebook_auth_code_exchange_spec.rb
+++ spec/stormpath/facebook_auth_code_exchange_spec.rb
@@ -9,9 +9,8 @@ describe Stormpath::Rails::FacebookAuthCodeExchange, vcr: true do
     let(:code) { Stormpath::Social::Helpers.mocked_authorization_code_for(:facebook) }
 
     it 'should return access token' do
-      allow(:exchanger_class).to receive(:access_token).and_return(Stormpath::Social::Helpers.mocked_access_token(:facebook))
-      expect(exchanger.access_token).to eq Stormpath::Social::Helpers.mocked_access_token(:facebook)['access_token']
+      allow(exchanger).to receive(:access_token).and_return(Stormpath::Social::Helpers.mocked_access_token_for(:facebook))
+      expect(exchanger.access_token).to eq Stormpath::Social::Helpers.mocked_access_token_for(:facebook)
     end
   end
-
 end
--- spec/stormpath/github_auth_code_exchange_spec.rb
+++ spec/stormpath/github_auth_code_exchange_spec.rb
@@ -9,9 +9,9 @@ describe Stormpath::Rails::GithubAuthCodeExchange, vcr: true do
     let(:code) { Stormpath::Social::Helpers.mocked_authorization_code_for(:github) }
 
     it 'should return access token' do
-      allow(:exchanger_class).to receive(:access_token).and_return(Stormpath::Social::Helpers.mocked_access_token(:github))
-      expect(exchanger.access_token).to eq Stormpath::Social::Helpers.mocked_access_token(:github)['access_token']
+      allow(exchanger).to receive(:access_token)
+        .and_return(Stormpath::Social::Helpers.mocked_access_token_for(:github))
+      expect(exchanger.access_token).to eq Stormpath::Social::Helpers.mocked_access_token_for(:github)
     end
   end
-
 end
