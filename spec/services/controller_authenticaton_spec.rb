07b94255bf1148b4942a21de15e9f7ba36c56745
Marko Ćilimković <marko.cilimkovic@infinum.hr>
2016-09-28 10:56:21 +0200

    Use random unencoded string as api key id and secret for the basic authentication specs

--- spec/services/controller_authenticaton_spec.rb
+++ spec/services/controller_authenticaton_spec.rb
@@ -536,7 +536,7 @@ describe Stormpath::Rails::ControllerAuthentication, vcr: true, type: :service d
     end
 
     describe 'with un encoded api key and secret' do
-      let(:credentials) { "#{api_key.id}:#{api_key.secret}" }
+      let(:credentials) { "#{unencodedapikeyid}:#{unencodedapikeysecret}" }
 
       it 'raises an UnauthenticatedRequest error' do
         expect do
