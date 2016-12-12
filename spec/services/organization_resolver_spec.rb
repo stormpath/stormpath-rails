53c836711572c3472c4294db16a56014ebe84a9c
Marko Ćilimković <marko.cilimkovic@infinum.hr>
2016-12-12 14:28:57 +0100

    Fix organization resolver specs for travis

--- spec/services/organization_resolver_spec.rb
+++ spec/services/organization_resolver_spec.rb
@@ -1,16 +1,23 @@
 require 'spec_helper'
 
 describe Stormpath::Rails::OrganizationResolver, vcr: true do
-  let(:application) { test_application }
+  let(:application) { test_client.applications.create(attributes_for(:application)) }
   let(:organization) { test_client.organizations.create(attributes_for(:organization)) }
   let(:resolver) do
     Stormpath::Rails::OrganizationResolver.new(request, org_name_key)
   end
+  let(:config) { Stormpath::Rails::Configuration }
   let(:request) { OpenStruct.new(subdomain: subdomain) }
   let(:org_name_key) { nil }
 
-  before { map_account_store(application, organization, 10, false, false) }
-  after { organization.delete }
+  before do
+    allow_any_instance_of(config).to receive(:application).and_return(application)
+    map_account_store(application, organization, 0, true, false)
+  end
+  after do
+    organization.delete
+    application.delete
+  end
 
   describe 'existing organization and mapped to application' do
     context 'from host' do
