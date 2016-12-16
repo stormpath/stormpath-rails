68ab4060cc0ee059083194f50524491d50470360
Marko Ćilimković <marko.cilimkovic@infinum.hr>
2016-12-16 12:58:45 +0100

    Add client specs where base path is not set

--- spec/stormpath/client_spec.rb
+++ spec/stormpath/client_spec.rb
@@ -1,18 +1,27 @@
 require 'spec_helper'
 
 describe Stormpath::Rails::Client, vcr: true do
-  let(:base_url) { 'https://eu.stormpath.io' }
+  let(:default_base_url) { 'https://api.stormpath.com/v1' }
+  let(:custom_base_url) { 'https://eu.stormpath.io' }
   let(:stormpath_client) { Stormpath::Rails::Client }
   let(:api_key) { Stormpath::Rails::ApiKey.new }
 
   describe 'base url set' do
     before do
       stormpath_client.connection = nil
-      allow(stormpath_client).to receive(:base_url).and_return(base_url)
+      allow(stormpath_client).to receive(:base_url).and_return(custom_base_url)
     end
 
     it 'should instantiate client with custom base url set in data store' do
-      expect(Stormpath::Rails::Client.client.data_store.base_url).to eq base_url
+      expect(Stormpath::Rails::Client.client.data_store.base_url).to eq custom_base_url
+    end
+  end
+
+  describe 'base url not set' do
+    before { stormpath_client.connection = nil }
+
+    it 'should instantiate client with default base url set in data store' do
+      expect(Stormpath::Rails::Client.client.data_store.base_url).to eq default_base_url
     end
   end
 end
